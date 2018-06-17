90cc7dea0bc2de37a4006e19503242d2326659d6
sunneepark <hl2ozs@naver.com>
2018-06-16 18:29:04 -0700

    more efficient for thread

--- Tetris++/src/com/tetris/rhythm/Note.java
+++ Tetris++/src/com/tetris/rhythm/Note.java
@@ -9,7 +9,6 @@ public class Note extends Thread{
 
 	public static final int NOTE_SPEED = 5;
 	public static final int SLEEP_TIME = 10;
-	Thread t;
 	
 	private Image noteImage = new ImageIcon("./src/image/note.png").getImage();
 	public int x, count, y =0;
@@ -23,7 +22,6 @@ public class Note extends Thread{
 		//this.y = y;
 		this.time = time;
 		this.space = space;
-		t=new Thread(this);
 	}
 
 	public void screenDraw(Graphics g) {
@@ -44,17 +42,15 @@ public class Note extends Thread{
 	
 	public void start_drop() {
 		this.drop_judge=true;
-		t.start();
 	}
 	public void stop_drop() {
 		this.drop_judge=false;
-		t.stop();
 	}
 	public void run() {
 		 try {
 			while(this.drop_judge) {
 			          this.drop();
-			          t.sleep(10);
+			          Thread.sleep(10);
 			 }
 			 
 	      }catch(Exception e) {
