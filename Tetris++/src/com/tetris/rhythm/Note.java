517722511f849a11a2b97c3e7529e07f9370a3e3
인택 조 <dlsxor21c@naver.com>
2018-06-18 14:48:03 +0900

    fix

--- Tetris++/src/com/tetris/rhythm/Note.java
+++ Tetris++/src/com/tetris/rhythm/Note.java
@@ -7,56 +7,60 @@ import javax.swing.*;
 
 public class Note extends Thread{
 
-	public static final int NOTE_SPEED = 5;
-	public static final int SLEEP_TIME = 10;
-	
-	private Image noteImage = new ImageIcon("./src/image/note.png").getImage();
-	public int x, count, y =0;
-	private int time;
-	public boolean space = false;
-	public boolean drop_judge=false;
-	
+   public static final int NOTE_SPEED = 5;
+   public static final int SLEEP_TIME = 10;
+   Thread t;
+   
+   private Image noteImage = new ImageIcon("./src/image/note.png").getImage();
+   public int x, count, y =0;
+   private int time;
+   public boolean space = false;
+   public boolean drop_judge=false;
+   
 
-	public Note(int x, int y, int time, boolean space) {
-		this.x = x;
-		//this.y = y;
-		this.time = time;
-		this.space = space;
-	}
+   public Note(int x, int y, int time, boolean space) {
+      this.x = x;
+      //this.y = y;
+      this.time = time;
+      this.space = space;
+      t=new Thread(this);
+   }
 
-	public void screenDraw(Graphics g) {
-		g.drawImage(noteImage, x, y, null);
-	}
-	
-	public void drop() {
-		
-		y += NOTE_SPEED;
-	}
-	
-	public int getTime() {
-		return time;
-	}
-	public int getY() {
-		return y;
-	}
-	
-	public void start_drop() {
-		this.drop_judge=true;
-	}
-	public void stop_drop() {
-		this.drop_judge=false;
-	}
-	public void run() {
-		 try {
-			while(this.drop_judge) {
-			          this.drop();
-			          Thread.sleep(10);
-			 }
-			 
-	      }catch(Exception e) {
+   public void screenDraw(Graphics g) {
+      g.drawImage(noteImage, x, y, null);
+   }
+   
+   public void drop() {
+      
+      y += NOTE_SPEED;
+   }
+   
+   public int getTime() {
+      return time;
+   }
+   public int getY() {
+      return y;
+   }
+   
+   public void start_drop() {
+      this.drop_judge=true;
+      t.start();
+   }
+   public void stop_drop() {
+      this.drop_judge=false;
+      t.stop();
+   }
+   public void run() {
+       try {
+         while(this.drop_judge) {
+                   this.drop();
+                   t.sleep(10);
+          }
+          
+         }catch(Exception e) {
 
-	      }
-	}
-	
-	
+         }
+   }
+   
+   
 }
